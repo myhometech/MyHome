14e080eb9fcbfcaba16014dfe276b9e1762fc41c
myhometech <simon@myhome-tech.com>
2025-09-03 21:19:52 +0000

    wouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, useLocation } from "wouter";
+import { Route, Switch, Link, useLocation } from 'wouter'
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";

c9ee38fdd156dbf57d2c135a4f472db25d146e77
myhometech <simon@myhome-tech.com>
2025-09-03 21:23:21 +0000

    app update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, Switch, Link, useLocation } from 'wouter'
 import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// optional:
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { useAuth } from "@/hooks/useAuth";
